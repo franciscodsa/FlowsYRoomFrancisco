780089c881883a872c14b4d87f307aca0a46c080
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2024-01-11 14:22:59 +0100

    commit

--- app/src/main/java/com/example/flowsyroomfrancisco/utils/TokenManager.kt
+++ app/src/main/java/com/example/flowsyroomfrancisco/utils/TokenManager.kt
@@ -10,12 +10,20 @@ import kotlinx.coroutines.flow.map
 class TokenManager(private val context: Context) {
     companion object{
         private val TOKEN_KEY = stringPreferencesKey("jwt_token")
+        private val REFRESH_TOKEN_KEY = stringPreferencesKey("refresh_jwt_token")
     }
 
     fun getToken(): Flow<String?>{
         return context.dataStore.data.map {
             preferences ->
             preferences[TOKEN_KEY]
+        }
+    }
+
+    fun getRefreshToken(): Flow<String?>{
+        return context.dataStore.data.map {
+            preferences ->
+            preferences[REFRESH_TOKEN_KEY]
         }
     }
     suspend fun saveToken(token: String) {
